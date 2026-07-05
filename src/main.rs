b8c7c74dcf7d7d8fb27a7953ffb35d9ffe5bee30
Mandrican Andrei <13387367+Andrei1524@users.noreply.github.com>
2026-07-05 23:47:45 +0300

    init server & import tcp listener method

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+use http::Method;
+use http::Request;
+
+use server::Server;
+
+
+mod server;
+mod http;
+
+fn main() {
+    let server = Server::new("127.0.0.01:8080".to_string());
+
+    server.run();
+}
